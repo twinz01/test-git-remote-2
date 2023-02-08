38dee1b44e981d3ec1afea3d1c992170cf573c9c
twinz01 <arylzan.astara@gmail.com>
2023-02-08 07:03:31 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quae illum similique, totam eos ex nam odio reprehenderit doloremque eius nostrum sit accusantium iusto maxime repellat! Magni, tempora sunt? Eum!</h1>
+    
+</body>
+</html>
